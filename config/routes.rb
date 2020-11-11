baebae0e432970912d2827099ed4cc741f59cc67
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-11 15:20:29 +0100

    yelp with my css do not judge me

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show, :destroy] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  root to: 'restaurants#index'
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Best bao", address: "Blecker Street 221b", phone_number: "0886754", category: "chinese")
+Restaurant.create(name: "Waffle's Factory", address: "Machelsesteenweg 267", phone_number: "07746754", category: "belgian")
+Restaurant.create(name: "Mario's Pizza", address: "Via Piave 130", phone_number: "0666954", category: "italian")
+Restaurant.create(name: "La Grande Evasion", address: "40 rue de la pompe", phone_number: "0671009789", category: "french")
+Restaurant.create(name: "Hittori", address: "216-1079, Fukuyoshidai", phone_number: "81710153097", category: "japanese")
